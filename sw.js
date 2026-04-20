262b5cbccb101d62fecb43407df248c0bbb65e45
stutishah10 <stushah1025@gmail.com>
2026-04-20 16:09:18 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,12 @@
 const CACHE_NAME = 'medistock-v1';
 const urlsToCache = [
   '/',
-  '/index.html'
+  '/index.html',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.6.0/jspdf.plugin.autotable.min.js'
 ];
 
 self.addEventListener('install', event => {
